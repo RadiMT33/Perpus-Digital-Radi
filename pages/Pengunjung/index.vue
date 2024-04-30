2c5981c87765c03e466aa5e2ba74542dc6fb7c41
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2024-04-30 11:10:03 +0700

    index.vue

--- pages/Pengunjung/index.vue
+++ pages/Pengunjung/index.vue
@@ -1,50 +1,58 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
-                <div class="my-3">
-                    <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter....">
-                </div>
-                <div class="my-3 text-muted">Menampilkan 1 dari 1</div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <td>#</td>
-                            <td>NAMA</td>
-                            <td>KEANGGOTAAN</td>
-                            <td>WAKTU</td>
-                            <td>KEPERLUAN</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(visitors,i) in visitors" :key="i">
-                            <td>{{ i+1 }}.</td>
-                            <td>{{ visitors.nama }}</td>
-                            <td>{{ visitors.Keanggotaan.nama }}</td>
-                            <td>{{ visitors.tanggal }}, {{ visitors.waktu }}</td>
-                            <td>{{ visitors.Keperluan.nama }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <nuxt-link to="http://localhost:3000/">
-                        <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">Kembali</button>
-                </nuxt-link>
-            </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
         </div>
+        <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(visitor,i) in visitors" :key="i">
+              <td>{{ i+1 }}.</td>
+              <td>{{ visitor.nama }}</td>
+              <td>{{ visitor.keanggotaan.nama }}</td>
+              <td>{{ visitor.tanggal }}, {{ visitor.waktu }}</td>
+              <td>{{ visitor.keperluan.nama }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+    <NuxtLink to="/pengunjung/tambah">
+      <button type="submit" class="btn btn-lg rounded-5 px-5">Kembali</button>
+    </NuxtLink>
+  </div>
 </template>
-
 <script setup>
 const supabase = useSupabaseClient()
 
 const visitors = ref([])
 
-const getPengunjung =async () => {
-    const { data, error } = await supabase.from('Pengunjung').select('*, Keanggotaan(*), Keperluan(*)')
-    if(data) visitors.value = data
+const getPengunjung = async () => {
+  const { data, error } = await supabase.from('pengunjung').select(`*, keanggotaan(*), keperluan(*)`)
+  if(data) visitors.value = data
 }
-onMonted(() => {
-    getPengunjung()
+
+onMounted(() => {
+  getPengunjung()
 })
 </script>
+<style scoped>
+.btn{
+  background-color: #ffd0d0f2;
+}
+.form-control{
+  background-color: #ffd0d0f2;
+}
+</style>
