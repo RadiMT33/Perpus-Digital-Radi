ab1be7b746288fdf36b7490af700bcca4f2f6462
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2024-04-30 11:03:39 +0700

    default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="container-fluid">
-        <Header/>
-        <slot/>
+  <div>
+    <div class=".container-fluid">
+      <Header />
+      <slot />
     </div>
+  </div>
 </template>
