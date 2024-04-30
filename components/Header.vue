3455ec2da286c73c5e001c0b53f6dfff48551be4
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2024-04-30 10:53:58 +0700

    Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,32 @@
 <template>
-    <div class="row p-3 bg-primary text-white">
-        <div class="col-lg-1">
-            <img src="../assets/img/LOGO-SMK4.png" class="logo">
-        </div>
-        <div class="col-lg-11 kj">
-            <h1>Perpus Digital</h1>
-            <address>
-                SMK NEGERI 4 KOTA TASIKMALAYA<br>
-                Jl.Depok, Kel.Sukamenak, Kec.Purbaratu.
-            </address>
-        </div>
+  <div class="header">
+    <div class="h-img">
+      <img src="~/assets/img/logo-smkn4.png" alt="logo smkn 4">
     </div>
+    <div class="h-text">
+    <div class="t-judul">
+      <h1>PERPUSTAKAAN DIGITAL</h1>
+      <address>
+        SMKN 4 Tasikmalaya
+        <p>jln. Depok, sukamenak, purbaratu</p>
+      </address>
+    </div>
+  </div>
+  </div>
 </template>
 <style scoped>
-.kj {
-  font-family: "karla ital";
+.header{
+  height: 150px;
+  display: flex;
+  align-items: center;
+  background-color: #0075FF;
+  color: #ffffff;
 }
-.logo {
-  width: 70%;
+.h-img >  img {
+  width: 80px;
+  margin-left: 50px;
+}
+.t-judul{
+  margin-left: 40px;
 }
 </style>
