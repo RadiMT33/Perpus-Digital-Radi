824e2aee1581c22200fda31c4bbc1bbf1b1065cc
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2024-04-30 11:07:05 +0700

    index.vue

--- pages/Buku/index.vue
+++ pages/Buku/index.vue
@@ -1,55 +1,73 @@
+import { NuxtLink } from '#build/components';
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <h2 class="text-center my-4">BUKU</h2>
-                <div class="my-3">
-                    <input type="search" class="form-control form-control-lg rounded-5" placeholder="mau baca apa hari ini">
-                </div>
-                <div class="my-3 text-muted">Menampilkan 3 dari 3</div>
-                <div class="row">
-                    <div class="col-lg-2">
-                        <div class="card mb-3">
-                            <div class="card-body">
-                                <nuxt-link to="../Rincian">
-                                    <img src="~/assets/img/Buku1.jpeg" class="Buku" alt="Buku1">
-                                </nuxt-link>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2">
-                        <div class="card mb-3">
-                            <div class="card-body">
-                                <img src="~/assets/img/Buku2.jpg" class="Buku" alt="Buku2">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2">
-                        <div class="card mb-3">
-                            <div class="card-body">
-                                <img src="~/assets/img/Buku3.jpg" class="Buku" alt="Buku3">
-                            </div>
-                        </div>
-                    </div>
-                    <nuxt-link to="http://localhost:3000/">
-                        <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">KEMBALI</button>
-                    </nuxt-link>
-                </div>
-            </div>
+  <div class="container-fluid p-5">
+    <div class="row">
+      <div class="col-lg-12 -flex justify-content-around">
+        <div class="my-5">
+          <form @submit.prevent="getBuku">
+            <input 
+            v-model="keyword" 
+            type="search" 
+            class="form-control rounded-5" 
+            placeholder="Mau baca apa hari ini?">
+          </form>
         </div>
+        <div class="my-4 text-muted">menampilkan 30 dari 30</div>
+        <div class="row justify-content-evenly">
+          <div v-for="(buku,i) in buku" :key="i" class="col-lg-2">
+            <NuxtLink :to="`/buku/${buku.id}`">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <img :src="buku.cover" class="cover" :alt="buku.judul">
+                </div>
+              </div>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
     </div>
+    <NuxtLink to="/">
+      <button type="submit" class="btn btn-lg rounded-5 px-5">Kembali</button>
+    </NuxtLink>
+  </div>
 </template>
 
-<style scoped>
-.card-body {
-    text-decoration: none;
-    width: 100%;
-    height: 20em;
-    padding: 0;
+<script setup>
+const supabase = useSupabaseClient()
+
+const buku = ref([])
+
+const getBuku = async () => {
+  const { data, error } = await supabase
+  .from('buku')
+  .select('*, kategori(*)')
+  .ilike("judul", `%${keyword.value}%`);
+  if(data) buku.value = data;
 }
-.Buku {
-    width: 100%;
-    height: 100%;
-    object-position: 0 30; 
+
+onMounted(() => {
+  getBuku()
+});
+
+const keyword = ref("");
+</script>
+
+<style scoped>
+.card-body{
+  width: 100%;
+  height: 400px;
+  padding: 0;
+}
+.cover {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: 0 30;
+}
+.btn{
+  background-color: #ffd0d0f2;
+}
+.form-control{
+  background-color: #ffd0d0f2;
 }
 </style>
